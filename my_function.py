32e065e4089d8ecaabbbf5c2cb8df642a4be3f9f
LAPTOP-LHUOQ7VL\91700 <ranjan.intel@gmail.com>
2022-06-05 20:08:25 +0530

    update test 2

--- my_function.py
+++ my_function.py
@@ -12,9 +12,6 @@ def update_app():
     if os.name == "nt":
         print('This feature is not available for Windows users.. Sorry..')
     else:
-        command = "cd .."
+        command = "cd .. && rm -r my_project && git clone http://github.com/somyaranjan/my_project.git"
         os.system(command)
-        command = "rm -r my_project"
-        os.system(command)
-        command = "git clone http://github.com/somyaranjan/my_project.git"
-        os.system(command)
+        
